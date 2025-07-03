bad7523b5b9f926ff728cc369b833486dd8e6a0e
Andric Quinn Baticos <andric.baticos@GXCHLAP00696.local>
2025-07-03 15:45:55 +0800

    add: variable requirements

--- src/main/java/javabank/BankAccount.java
+++ src/main/java/javabank/BankAccount.java
@@ -2,8 +2,28 @@ package javabank;
 
 public class BankAccount {
 
-	BankAccount() {
+	private String accountNumber;
+	private String accountName;
+	private double availableBalance;
+
+	public BankAccount() {
 
 	}
+
+//	public void withdraw() {
+//
+//	}
+
+//	public void deposit() {
+//
+//	}
+
+//	public void displayInformation() {
+//
+//	}
+
+//	public String getAccountNumber() {
+//		return this.accountNumber;
+//	}
 
 }
