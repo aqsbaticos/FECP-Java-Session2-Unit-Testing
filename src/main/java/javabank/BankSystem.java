35a9e8b3cd69f9cd80b8adbff8e875bf7148523d
Andric Quinn Baticos <andric.baticos@GXCHLAP00696.local>
2025-07-03 16:21:17 +0800

    edit: exit code -> 0, add: viewAllAccounts()

--- src/main/java/javabank/BankSystem.java
+++ src/main/java/javabank/BankSystem.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class BankSystem {
 
 	static Scanner in = new Scanner(System.in);
-	ArrayList<BankAccount> bankAccounts= new ArrayList<>();
+	static ArrayList<BankAccount> bankAccounts= new ArrayList<>();
 
 	public static void main(String[] args) {
 
@@ -18,14 +18,16 @@ public class BankSystem {
 			System.out.println("3. Check Balance");
 			System.out.println("4. Deposit");
 			System.out.println("5. Withdraw");
-			System.out.println("6. Exit");
+			System.out.println("0. Exit");
 			System.out.print("Enter choice: ");
 				choice = in.next();
 
 			switch(choice) {
 				case "1":
+//					int result = createAccount();
 					break;
 				case "2":
+					viewAllAccounts();
 					break;
 				case "3":
 					break;
@@ -33,7 +35,7 @@ public class BankSystem {
 					break;
 				case "5":
 					break;
-				case "6":
+				case "0":
 					isRunning = false;
 					break;
 				default:
@@ -42,5 +44,35 @@ public class BankSystem {
 
 			System.out.println();
 		}
+	}
+
+//	private static int createAccount() {
+//		String accountName, accountNumber, initialDepositChoice;
+//		System.out.print("Enter Account Number: "); accountNumber = in.next();
+//		System.out.print("Enter Holder Name: "); accountName = in.next();
+//
+//		double initialDepositAmount; boolean checkInitialDepositInput = true;
+//		while(checkInitialDepositInput) {
+//			System.out.print("Initial deposit? (yes/no): "); initialDepositChoice = in.next();
+//			if(initialDepositChoice.toLowerCase().equals("yes")) {
+//				System.out.print("Enter initial deposit amount: "); initialDepositAmount = in.nextDouble();
+//				if (initialDepositAmount >= 0) {
+//					BankAccount newAccount = new BankAccount(accountNumber, accountName, initialDepositAmount);
+//					bankAccounts.add(newAccount);
+//					System.out.println("Account created successfully!");
+//				}
+//			} else if (initialDepositChoice.toLowerCase().equals("no")) {
+//				checkInitialDepositInput = false;
+//			}
+//		}
+//	}
+
+	private static void viewAllAccounts() {
+		System.out.println("=== View All Accounts ===");
+		for (int i=0; i<bankAccounts.size(); i++) {
+			bankAccounts.get(i).displayInformation();
+			System.out.println("");
+		}
+		System.out.println("===================== END");
 	}
 }
