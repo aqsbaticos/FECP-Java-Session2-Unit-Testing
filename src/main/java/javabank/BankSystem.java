1594595960289bd39f7057311cddc8d47f89ae86
Andric Quinn Baticos <andric.baticos@GXCHLAP00696.local>
2025-07-03 15:27:44 +0800

    initialize: ArrayList<BankAccount> bankAccounts

--- src/main/java/javabank/BankSystem.java
+++ src/main/java/javabank/BankSystem.java
@@ -1,6 +1,10 @@
 package javabank;
 
+import java.util.ArrayList;
+
 public class BankSystem {
+
+	ArrayList<BankAccount> bankAccounts= new ArrayList<>();
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
