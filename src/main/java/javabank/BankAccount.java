5a696ba69852f877d160cadb9cec8cc8e55ad578
Andric Quinn Baticos <andric.baticos@GXCHLAP00696.local>
2025-07-03 17:04:48 +0800

    remove: duplicate getAccountNumber

--- src/main/java/javabank/BankAccount.java
+++ src/main/java/javabank/BankAccount.java
@@ -41,8 +41,4 @@ public class BankAccount {
 		System.out.printf("Available Balance: %f\n", this.availableBalance);
 	}
 
-//	public String getAccountNumber() {
-//		return this.accountNumber;
-//	}
-
 }
